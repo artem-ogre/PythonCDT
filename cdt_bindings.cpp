070044e36242a8655671aa70f5fe648603db5ac0
artem-ogre <artem.ogre@gmail.com>
2022-05-03 12:41:39 +0200

    Add Python bindings and tests licensed under MPL-2

--- cdt_bindings.cpp
+++ cdt_bindings.cpp
@@ -1,10 +1,23 @@
-#include <CDT.h>
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include <CDT.h>
+#include <VerifyTopology.h>
+
+#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 
-using namespace CDT;
+#include <string>
+#include <sstream>
+
 namespace py = pybind11;
+
+using coord_t = double;
+using V2d = CDT::V2d<coord_t>;
+using Triangulation = CDT::Triangulation<coord_t>;
 
 PYBIND11_MODULE(PythonCDT, m)
 {
@@ -20,8 +33,134 @@ PYBIND11_MODULE(PythonCDT, m)
     // clang-format on
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+        m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+        m.attr("__version__") = "dev";
 #endif
+
+    m.attr("NO_NEIGHBOR") = py::int_(CDT::noNeighbor);
+    m.attr("NO_VERTEX") = py::int_(CDT::noVertex);
+
+    py::enum_<CDT::VertexInsertionOrder::Enum>(m, "VertexInsertionOrder")
+        .value("RANDOMIZED", CDT::VertexInsertionOrder::Randomized)
+        .value("AS_PROVIDED", CDT::VertexInsertionOrder::AsProvided);
+
+    py::enum_<CDT::IntersectingConstraintEdges::Enum>(
+        m, "IntersectingConstraintEdges")
+        .value("IGNORE", CDT::IntersectingConstraintEdges::Ignore)
+        .value("RESOLVE", CDT::IntersectingConstraintEdges::Resolve);
+
+    py::class_<V2d>(m, "V2d", py::buffer_protocol())
+        .def(py::init<int, int>(), py::arg("x"), py::arg("y"))
+        .def(py::init<coord_t, coord_t>(), py::arg("x"), py::arg("y"))
+        .def(py::init([](py::buffer b) {
+            // Request a buffer descriptor from Python
+            py::buffer_info info = b.request();
+            // Some sanity checks ...
+            if (info.format != py::format_descriptor<coord_t>::format())
+                throw std::runtime_error(
+                    "Incompatible format: expected a double array!");
+            if (info.ndim != 1)
+                throw std::runtime_error("Incompatible buffer dimension!");
+            // create from buffer
+            const coord_t* const ptr = static_cast<coord_t*>(info.ptr);
+            return V2d{ptr[0], ptr[1]};
+        }))
+        .def_readwrite("x", &V2d::x)
+        .def_readwrite("y", &V2d::y)
+        .def(
+            "__repr__",
+            [](const V2d& v) {
+                std::ostringstream oss;
+                oss << "V2d(" << v.x << ", " << v.y << ")";
+                return oss.str();
+            })
+        .def_buffer([](V2d& v) -> py::buffer_info {
+            return py::buffer_info(
+                &v,
+                sizeof(coord_t),
+                py::format_descriptor<coord_t>::format(),
+                1,
+                {2},
+                {sizeof(coord_t) * 2});
+        });
+    PYBIND11_NUMPY_DTYPE(V2d, x, y);
+
+    PYBIND11_NUMPY_DTYPE(CDT::Triangle, vertices, neighbors);
+    py::class_<CDT::Triangle>(m, "Triangle")
+        .def_readwrite("vertices", &CDT::Triangle::vertices)
+        .def_readwrite("neighbors", &CDT::Triangle::neighbors);
+
+    py::class_<CDT::Edge>(m, "Edge", py::buffer_protocol())
+        .def(
+            py::init<CDT::VertInd, CDT::VertInd>(),
+            py::arg("index_vert_a"),
+            py::arg("index_vert_b"))
+        .def(py::init([](py::buffer b) {
+            // Request a buffer descriptor from Python
+            py::buffer_info info = b.request();
+            // Some sanity checks ...
+            if (info.format != py::format_descriptor<CDT::VertInd>::format())
+                throw std::runtime_error(
+                    "Incompatible format: expected a CDT::VertInd array!");
+            if (info.ndim != 1)
+                throw std::runtime_error("Incompatible buffer dimension!");
+            // create from buffer
+            const CDT::VertInd* const ptr =
+                static_cast<CDT::VertInd*>(info.ptr);
+            return CDT::Edge(ptr[0], ptr[1]);
+        }))
+        .def_buffer([](CDT::Edge& e) -> py::buffer_info {
+            return py::buffer_info(
+                &e,
+                sizeof(CDT::VertInd),
+                py::format_descriptor<coord_t>::format(),
+                1,
+                {2},
+                {sizeof(CDT::VertInd) * 2});
+        })
+        .def_property_readonly("v1", &CDT::Edge::v1)
+        .def_property_readonly("v2", &CDT::Edge::v2)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def(py::hash(py::self))
+        .def("__repr__", [](const CDT::Edge& e) {
+            std::ostringstream oss;
+            oss << "Edge(" << e.v1() << ", " << e.v2() << ")";
+            return oss.str();
+        });
+
+    py::class_<Triangulation>(m, "Triangulation")
+        .def(
+            py::init<
+                CDT::VertexInsertionOrder::Enum,
+                CDT::IntersectingConstraintEdges::Enum,
+                coord_t>(),
+            py::arg("vertex_insertion_order"),
+            py::arg("intersecting_edges_strategy"),
+            py::arg("min_dist_to_constraint_edge"))
+        .def_readonly("vertices", &Triangulation::vertices)
+        .def_readonly("triangles", &Triangulation::triangles)
+        .def_readonly("fixed_edges", &Triangulation::fixedEdges)
+        .def_readonly("vertices_triangles", &Triangulation::vertTris)
+        .def(
+            "insert_vertices",
+            static_cast<void (Triangulation::*)(const std::vector<V2d>&)>(
+                &Triangulation::insertVertices),
+            py::arg("vertices"))
+        .def(
+            "insert_edges",
+            static_cast<void (Triangulation::*)(const std::vector<CDT::Edge>&)>(
+                &Triangulation::insertEdges),
+            py::arg("edges"))
+        .def("erase_super_triangle", &Triangulation::eraseSuperTriangle)
+        .def("erase_outer_triangles", &Triangulation::eraseOuterTriangles)
+        .def(
+            "erase_outer_triangles_and_holes",
+            &Triangulation::eraseOuterTrianglesAndHoles);
+
+    m.def(
+        "verify_topology",
+        &CDT::verifyTopology<coord_t>,
+        py::arg("triangulation"));
 }
---
+++ cdt_bindings_test.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+""" Tests for CDT Python bindings """
+
+import numpy as np
+import pytest
+import tempfile
+import hashlib
+
+import PythonCDT as cdt
+
+
+def test_constants() -> None:
+    """Test that constants have proper values"""
+    assert cdt.NO_NEIGHBOR == np.iinfo(np.uint32).max, "NO_NEIGHBOR constant has wrong value"
+    assert cdt.NO_VERTEX == np.iinfo(np.uint32).max, "NO_VERTEX constant has wrong value"
+
+
+def test_V2d() -> None:
+    """Test 2D vector"""
+    p = cdt.V2d(42, 42)
+    assert p.x == 42 and p.y == 42, "Error in constructing 2D vector with int"
+    p = cdt.V2d(42.0, 42.0)
+    assert p.x == 42 and p.y == 42, "Error in constructing 2D vector with float"
+    p = cdt.V2d(np.array([42., 42.]))
+    assert p.x == 42 and p.y == 42, "Error in constructing 2D vector with buffer protocol"
+
+    assert cdt.V2d(1.23, 2).__repr__() == "V2d(1.23, 2)", "Wrong __repr__ output for V2d"
+
+
+def test_Edge() -> None:
+    """Test Edge class"""
+    e = cdt.Edge(1, 2)
+    assert e.v1 == 1 and e.v2 == 2, "Constructed wrong edge"
+    e = cdt.Edge(2, 1)
+    assert e.v1 == 1 and e.v2 == 2, "Constructed wrong edge"
+    e = cdt.Edge(np.array([2, 1], dtype=np.uint32))
+    assert e.v1 == 1 and e.v2 == 2, "Constructed wrong edge"
+
+    assert cdt.Edge(1, 2).__repr__() == "Edge(1, 2)", "Wrong __repr__ output for Edge"
+
+
+def test_Triangulation() -> None:
+    """Test Triangulation class"""
+    t = cdt.Triangulation(cdt.VertexInsertionOrder.AS_PROVIDED, cdt.IntersectingConstraintEdges.IGNORE, 0.0)
+    assert len(t.vertices) == 0, "Wrong vertex count in empty triangulation"
+    assert len(t.triangles) == 0, "Wrong triangle count in empty triangulation"
+    assert len(t.fixed_edges) == 0, "Wrong fixed edge count in empty triangulation"
+    assert len(t.vertices_triangles) == 0, "Wrong vertices triangles count in empty triangulation"
+
+    vv = [cdt.V2d(-1, 0), cdt.V2d(0, 0.5), cdt.V2d(1, 0), cdt.V2d(0, -0.5)]
+    t.insert_vertices(vv)
+    assert len(t.vertices) == 7, "Wrong vertex count in triangulation"
+    assert len(t.triangles) == 9, "Wrong triangle count in triangulation"
+    assert len(t.fixed_edges) == 0, "Wrong fixed edge count in triangulation"
+    assert t.vertices_triangles, "Wrong vertices triangles count in triangulation"
+
+    ee = [cdt.Edge(0, 2)]
+    t.insert_edges(ee)
+    assert len(t.fixed_edges) == 1, "Wrong fixed edge count in triangulation"
+    assert cdt.Edge(0 + 3, 2 + 3) in t.fixed_edges, "Constraint edge was not properly added"
+
+    t.erase_super_triangle()
+    assert cdt.Edge(0, 2) in t.fixed_edges, "Constraint edge was not properly added"
+    assert len(t.vertices) == 4, "Wrong vertex count in triangulation"
+    assert len(t.triangles) == 2, "Wrong triangle count in triangulation"
+    assert len(t.fixed_edges) == 1, "Wrong fixed edge count in triangulation"
+    assert t.vertices_triangles, "Wrong vertices triangles count in triangulation"
+
+    #  Test resolving fixed edge intersections
+    t = cdt.Triangulation(cdt.VertexInsertionOrder.AS_PROVIDED, cdt.IntersectingConstraintEdges.RESOLVE, 0.0)
+    ee = [cdt.Edge(0, 2), cdt.Edge(1, 3)]
+    t.insert_vertices(vv)
+    t.insert_edges(ee)
+    t.erase_super_triangle()
+    assert len(t.vertices) == 5, "Wrong vertex count in triangulation"
+    assert len(t.triangles) == 4, "Wrong triangle count in triangulation"
+
+
+def test_verify_topology() -> None:
+    """Test verifying CDT topology"""
+    t = cdt.Triangulation(cdt.VertexInsertionOrder.AS_PROVIDED, cdt.IntersectingConstraintEdges.RESOLVE, 0.0)
+    t.insert_vertices([cdt.V2d(-1, 0), cdt.V2d(0, 0.5), cdt.V2d(1, 0), cdt.V2d(0, -0.5)])
+    t.insert_edges([cdt.Edge(0, 2), cdt.Edge(1, 3)])
+    assert cdt.verify_topology(t), "Verifying topology produced wrong result"
+
+
+def save_triangulation_as_off(t: cdt.Triangulation, off_file) -> None:
+    with open(off_file, "w") as f:
+        f.write(f"OFF\n")
+        f.write(f"{len(t.vertices)} {len(t.triangles)} 0\n")
+        for v in t.vertices:
+            f.write(f"{v.x} {v.y} 0\n")
+        for tri in t.triangles:
+            vv = tri.vertices
+            f.write(f" 3 {int(vv[0])} {int(vv[1])} {int(vv[2])}\n")
+
+
+def read_input_file(input_file):
+    with open(input_file, "r") as f:
+        n_verts, n_edges = tuple([int(s) for s in f.readline().split()])
+        verts = [cdt.V2d(*(float(s) for s in f.readline().split())) for _ in range(n_verts)]
+        edges = [cdt.Edge(*(int(s) for s in f.readline().split())) for _ in range(n_edges)]
+        return verts, edges
+
+
+def test_triangulate_input_file() -> None:
+    vv, ee = read_input_file("CDT/visualizer/data/Constrained Sweden.txt")
+    t = cdt.Triangulation(cdt.VertexInsertionOrder.AS_PROVIDED, cdt.IntersectingConstraintEdges.RESOLVE, 0.0)
+    t.insert_vertices(vv)
+    t.insert_edges(ee)
+    t.erase_outer_triangles_and_holes()
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        off_file = f"{tmp_dir}/cdt.off"
+        save_triangulation_as_off(t, off_file)
+        with open(off_file, 'rb') as f:
+            assert hashlib.md5(f.read()).hexdigest() == '29110b24778e3e7d60bd2e99451fd6de', "Wrong OFF file contents"
